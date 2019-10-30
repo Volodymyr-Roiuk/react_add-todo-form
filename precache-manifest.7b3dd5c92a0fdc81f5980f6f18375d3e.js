8ff6701b35e0407e0600769ed4fd350e4f2a7f4b
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-30 14:08:15 +0300

    Updates

---
+++ precache-manifest.7b3dd5c92a0fdc81f5980f6f18375d3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2c735a0e145250eca4bd8201490918",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "981d86395efec4b3056b",
+    "url": "/react_add-todo-form/static/css/main.28ab2ff2.chunk.css"
+  },
+  {
+    "revision": "9ea70f0bc0998e9fc926",
+    "url": "/react_add-todo-form/static/js/2.5a352b83.chunk.js"
+  },
+  {
+    "revision": "981d86395efec4b3056b",
+    "url": "/react_add-todo-form/static/js/main.a705e0b2.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
